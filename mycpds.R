45bebbbbd160f8b499a321974a9d826fa61cd9cd
itamburi <itamburi@uci.edu>
2026-02-02 16:41:26 -0800

    organized directories to set up for hmdb script

--- mycpds.R
+++ mycpds.R
@@ -7,4 +7,6 @@ mycpds = read.csv("mycpds.csv")
 
 set.seed(123)  # for reproducibility
 mycpds_short <- mycpds[sample(nrow(mycpds), 50), ]
+row.names(mycpds_short) <- NULL
+mycpds_short$X = NULL
 write.csv(mycpds_short, "mycpds_short.csv")
